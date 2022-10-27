bfd03fd9698908faf639b1e62c94149778c24e57
YuJian920 <HAO52013141@Gmail.com>
2022-10-27 21:53:42 +0800

    修复 Vercel 部署报错

---
+++ src/layout/index.tsx
@@ -0,0 +1,15 @@
+import { useRoutes } from "react-router-dom";
+import routerConfig from "../router";
+import Navigation from "./components/Navigation";
+
+export default () => {
+  const routerElement = useRoutes(routerConfig);
+  return (
+    <>
+      <Navigation />
+      <main className="w-full overflow-auto fixed inset-y-0 right-0 pt-16 pb-24 pr-[10vw] pl-[10vw]">
+        {routerElement}
+      </main>
+    </>
+  );
+};
