2ef9a4f37c3c5f1cec1e10ca04ad616c914e25b0
YuJian920 <HAO52013141@Gmail.com>
2022-10-27 10:37:15 +0800

    引入路由实现跳转

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,14 @@
-import Home from "../pages/Home";
-import PlayList from "../pages/PlayList";
+import { useRoutes } from "react-router-dom";
+import routerConfig from "../router";
 import Navigation from "./components/Navigation";
 
 export default () => {
+  const routerElement = useRoutes(routerConfig);
   return (
     <>
       <Navigation />
       <main className="w-full overflow-auto fixed inset-y-0 right-0 pt-16 pb-24 pr-[10vw] pl-[10vw]">
-        <PlayList />
-        {/* <Home /> */}
+        {routerElement}
       </main>
     </>
   );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,1 +1,16 @@
-export {}
+import type { RouteObject } from "react-router-dom";
+import Home from "../pages/Home";
+import Playlist from "../pages/Playlist";
+
+const routerConfig: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/playlist/:id",
+    element: <Playlist />,
+  },
+];
+
+export default routerConfig;
