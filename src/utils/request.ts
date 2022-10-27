99e106b2a7c4c36795599da1b9a5370e2d188f80
YuJian920 <HAO52013141@Gmail.com>
2022-10-27 10:33:22 +0800

    配置开发代理服务器

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import { stringify } from "qs";
 
-const baseURL = "http://localhost:3000";
+const baseURL = "/api";
 
 interface RequestOptions {
   data?: object | string;
@@ -10,7 +10,7 @@ export default async <T>(
   endpoint: string,
   option: RequestOptions = {}
 ): Promise<Awaited<T>> => {
-  const { data } = option;
+  const data = option.data || option;
   const path = `${baseURL}${endpoint}` + (data ? `?${stringify(data)}` : "");
   return fetch(path)
     .then(async (response) => {
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://netease.yujian.icu",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
